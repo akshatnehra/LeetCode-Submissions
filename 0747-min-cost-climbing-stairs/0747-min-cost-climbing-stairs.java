4e128e830bdb8b30c866fcad96fec6ddedae8674
Akshat Nehra <49430983+akshatnehra@users.noreply.github.com>
2023-08-24 23:03:03 +0530

    Time: 0 ms (100%), Space: 42.8 MB (87.74%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.java
@@ -1,30 +1,16 @@
 class Solution {
-    // Rec + memo
+    // Tabulation
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length+1;
-        int dp[] = new int[n];
+        int n = cost.length;
+        int dp[] = new int[n+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
 
-        for(int i=0; i<n; i++) {
-            dp[i] = -1;
+        for(int i=2; i<n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
         }
 
-        minCostClimbingStairsHelper(cost, cost.length, dp);
-
-        return dp[n-1];
-    }
-
-    public int minCostClimbingStairsHelper(int cost[], int index, int dp[]) {
-        if(index == 0 || index == 1) {
-            return cost[index];
-        }
-
-        if(dp[index] != -1) {
-            return dp[index];
-        }
-
-        int prev1 = index-1  > 1 ? minCostClimbingStairsHelper(cost, index-1, dp)+cost[index-1] : minCostClimbingStairsHelper(cost, index-1, dp);
-        int prev2 = index-2  > 1 ? minCostClimbingStairsHelper(cost, index-2, dp)+cost[index-2] : minCostClimbingStairsHelper(cost, index-2, dp);
-        dp[index] = Math.min(prev1, prev2);
-        return dp[index];
+        dp[n] = Math.min(dp[n-1], dp[n-2]);
+        return dp[n];
     }
 }
